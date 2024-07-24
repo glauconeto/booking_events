010a3f73cab429549d323d957f221841c9d074ef
glauconeto <glauco.neto@tutanota.com>
2024-07-24 12:36:22 -0300

    Adicionadas configurações do firebase

--- assets/js/db.js
+++ assets/js/db.js
@@ -1,3 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBh17nbtOsqmN0LV84BeBp_Rso1XPqv7VM",
+  authDomain: "bookingevents-5db77.firebaseapp.com",
+  projectId: "bookingevents-5db77",
+  storageBucket: "bookingevents-5db77.appspot.com",
+  messagingSenderId: "768081108112",
+  appId: "1:768081108112:web:c9ca975b48acd162f6cc15"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase()
+
 db.collection('events').onSnapshot(snapshot => {
   // Handle the latest event
   const newestEvent = snapshot.docChanges()[0].doc.data()
