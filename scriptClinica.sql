8c663511b9ff876147f9fb4b9dd1e78a90ef8f16
glauconeto <glauco.neto@tutanota.com>
2024-08-13 20:19:09 -0300

    Estrutura da página de listagem de agendamentos

--- scriptClinica.sql
+++ scriptClinica.sql
@@ -5,16 +5,14 @@ CREATE TABLE `usuario` (
 	`senha` VARCHAR(100),
 );
 
-
 CREATE TABLE `agendamento` (
 	`id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	`nome` VARCHAR(70),
+	`nome` VARCHAR(70) NOT NULL,
+	`telefone` VARCHAR(20) NOT NULL,
 	`dataHora` TIMESTAMP NOT NULL,
-	`idUsuario` INTEGER NOT NULL,
 	`idAdministrador` INTEGER,
 	PRIMARY KEY(`id`)
 );
-
 
 CREATE TABLE `administrador` (
 	`id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -22,7 +20,6 @@ CREATE TABLE `administrador` (
 	`senha` VARCHAR(255) NOT NULL,
 	PRIMARY KEY(`id`)
 );
-
 
 CREATE TABLE `informações usuario` (
 	`id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -33,19 +30,14 @@ CREATE TABLE `informações usuario` (
 	PRIMARY KEY(`id`)
 );
 
-
 CREATE TABLE `preco` (
 	`id` INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`nomeProcedimento` VARCHAR(100),
 	`preco` DECIMAL(6, 2),
-	`descricao` TEXT(10000),
+	`descricao` TEXT(100),
 	PRIMARY KEY(`id`)
 );
 
-
-ALTER TABLE `usuario`
-ADD FOREIGN KEY(`id`) REFERENCES `agendamento`(`idUsuario`)
-ON UPDATE NO ACTION ON DELETE CASCADE;
 ALTER TABLE `administrador`
 ADD FOREIGN KEY(`id`) REFERENCES `agendamento`(`idAdministrador`)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
